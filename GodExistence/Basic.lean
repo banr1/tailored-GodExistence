05055b46e067016c39b03c497df8116541184810
SnO2WMaN <me@sno2wman.net>
2025-12-29 19:19:42 +0900

    done

--- GodExistence/Basic.lean
+++ GodExistence/Basic.lean
@@ -1,186 +1,260 @@
 import Mathlib.Data.Set.Basic
+import Mathlib.Tactic
 
 namespace GodExistence
 
-structure KripkeModel where
+structure Model where
   World : Type u
   Rel : World → World → Prop
 
 
-namespace KripkeModel
+namespace Model
 
-variable {K : KripkeModel}
+variable {K : Model}
 
-instance : CoeSort KripkeModel (Type u) := ⟨KripkeModel.World⟩
-instance : CoeFun KripkeModel (λ K => K.World → K.World → Prop) := ⟨KripkeModel.Rel⟩
+instance : CoeSort Model (Type u) := ⟨Model.World⟩
+instance : CoeFun Model (λ K => K.World → K.World → Prop) := ⟨Model.Rel⟩
 
-class IsReflexive (K : KripkeModel) : Prop where
+class IsReflexive (K : Model) : Prop where
   refl : ∀ {x : K}, K x x
 export IsReflexive (refl)
 attribute [grind .] refl
 
-class IsTransitive (K : KripkeModel) : Prop where
+class IsTransitive (K : Model) : Prop where
   trans: ∀ {x y z : K}, K x y → K y z → K x z
 export IsTransitive (trans)
 attribute [grind <=] trans
 
-class IsSymmetric (K : KripkeModel) : Prop where
+class IsSymmetric (K : Model) : Prop where
   symm : ∀ {x y : K}, K x y → K y x
 export IsSymmetric (symm)
 attribute [grind <=] symm
 
+class IsEuclidean (K : Model) : Prop where
+  eucl : ∀ {x y z : K}, K x y → K x z → K y z
+export IsEuclidean (eucl)
 
-alias IsKT := IsReflexive
-alias IsK4 := IsTransitive
-alias IsKB := IsSymmetric
+class IsKTB (K : Model) extends K.IsReflexive, K.IsSymmetric where
+class IsKB4 (K : Model) extends K.IsSymmetric, K.IsTransitive where
+class IsS4 (K : Model) extends K.IsReflexive, K.IsTransitive where
+class IsS5 (K : Model) extends K.IsReflexive, K.IsSymmetric, K.IsTransitive where
 
-class IsS4 (K : KripkeModel) extends K.IsKT, K.IsK4 where
-class IsS5 (K : KripkeModel) extends K.IsS4, K.IsKB where
+instance [K.IsKB4] : K.IsEuclidean where
+  eucl {x y z} Rxy Rxz := by
+    exact K.trans (K.symm Rxy) Rxz;
 
-end KripkeModel
+instance [K.IsS5] : K.IsKB4 where
+
+instance [K.IsS5] : K.IsEuclidean := instIsEuclideanOfIsKB4
+
+end Model
 
 
+abbrev Formula (K : Model) := Set K
 
-inductive Formula (α : Type) : Type
-  | falsum : Formula α
-  | verum : Formula α
-  | and : Formula α → Formula α → Formula α
-  | or : Formula α → Formula α → Formula α
-  | all (P : α → Formula α) : Formula α
-  | ex (P : α → Formula α) : Formula α
-  | box : Formula α → Formula α
-  | dia : Formula α → Formula α
-
-abbrev Predicate (α : Type) := α → Formula α
+/-- Statement about being (`α`) -/
+abbrev Property (α) (K : Model) := α → Formula K
 
 namespace Formula
 
-variable {α} {K : KripkeModel}
+variable {K : Model} {x : K} {φ ψ : Formula K} {Φ : Property α K}
 
-notation "⊥" => falsum
-notation "⊤" => verum
-infixl:75 " ⋏ " => and
-infixl:74 " ⋎ " => or
-prefix:80 "□" => box
-prefix:80 "◇" => dia
+@[simp] def falsum : Formula K := λ _ => False
+notation:max "⊥ₘ" => falsum
 
--- notation:78 "∀' " P => all P
--- notation:78 "∃' " P => ex P
+@[simp] def verum : Formula K := λ _ => True
+notation:max "⊤ₘ" => verum
 
-notation:78 "∀' " P "," a => all P a
-notation:78 "∃' " P "," a => ex P a
-
-@[grind]
-def neg : Formula α → Formula α
-  | ⊥ => ⊤
-  | ⊤ => ⊥
-  | φ ⋏ ψ => neg φ ⋎ neg ψ
-  | φ ⋎ ψ => neg φ ⋏ neg ψ
-  -- | ∀' P => ∃' (λ x => neg (P x))
-  -- | ∃' P => ∀' (λ x => neg (P x))
-  | ∀' P, φ => ∃' (λ x => neg (P x)), φ
-  | ∃' P, φ => ∀' (λ x => neg (P x)), φ
-  | □φ => ◇(neg φ)
-  | ◇φ => □(neg φ)
+@[grind] def neg (φ : Formula K) : Formula K := λ x => ¬(φ x)
 prefix:80 "∼" => neg
 
-abbrev imp (φ ψ : Formula α) := ∼φ ⋎ ψ
-infixr:76 " ➝ " => imp
+@[simp] lemma eq_negverum_falsum : ∼(⊤ₘ : Formula K) = ⊥ₘ := by funext x; simp [verum, falsum, neg];
+@[simp] lemma eq_negfalsum_verum : ∼(⊥ₘ : Formula K) = ⊤ₘ := by funext x; simp [verum, falsum, neg];
 
-abbrev iff (φ ψ : Formula α) := (φ ➝ ψ) ⋏ (ψ ➝ φ)
-infix:70 " ⭤ " => iff
+@[simp, grind .] lemma not_falsum : ¬(⊥ₘ x) := by simp [falsum];
+@[simp, grind .] lemma always_verum : ⊤ₘ x := by simp [verum];
+
+
+@[grind] def and (φ ψ : Formula K) : Formula K := λ x => φ x ∧ ψ x
+infixl:75 " ⋏ " => and
+
+@[grind] def or (φ ψ : Formula K) : Formula K := λ x => φ x ∨ ψ x
+infixl:74 " ⋎ " => or
+
+@[simp, grind] def imp (φ ψ : Formula K) : Formula K := λ x => φ x → ψ x
+infixr:66 " ➝ " => imp
+
+@[grind] def iff (φ ψ : Formula K) : Formula K := λ x => φ x ↔ ψ x
+infix:65 " ⭤ " => iff
+
+@[grind] def all (Φ : Property α K) : Formula K := λ x => ∀ a, Φ a x
+prefix:80 "∀'" => all
+
+@[grind] def ex (Φ : Property α K) : Formula K := λ x => ∃ a, Φ a x
+prefix:80 "∃'" => ex
+
+@[grind] def box (φ : Formula K) : Formula K := λ x => ∀ y, K x y → φ y
+prefix:80 "□" => box
+
+@[grind] def dia (φ : Formula K) : Formula K := λ x => ∃ y, K x y ∧ φ y
+prefix:80 "◇" => dia
+
+
+@[grind =] lemma eq_negbox_dianeg : ∼(□φ) = ◇(∼φ) := by funext x; grind;
+@[grind =] lemma eq_negall_exneg : ∼(∀' Φ) = ∃' (λ a => ∼(Φ a)) := by funext x; grind;
+
+
+def Valid (φ : Formula K) : Prop := ∀ x, φ x
+prefix:60 "⊧ " => Valid
 
 end Formula
 
 
-abbrev Predicate.neg (P : Predicate α) : Predicate α := λ x => ∼(P x)
-prefix:85 "∼" => Predicate.neg
 
-@[grind]
-def Forces {K : KripkeModel} (x : K) : Formula α → Prop
-  | ⊥ => False
-  | ⊤ => True
-  | φ ⋏ ψ => Forces x φ ∧ Forces x ψ
-  | φ ⋎ ψ => Forces x φ ∨ Forces x ψ
-  | ∀' P, φ => ∀ a : α, Forces x (P a) → Forces x φ
-  | ∃' P, φ => ∃ a : α, Forces x (P a) ∧ Forces x φ
-  -- | ∀' P, a => ∀ a, Forces x (P a)
-  -- | ∃' P, a => ∃ a, Forces x (P a)
-  | □φ => ∀ y, K x y → Forces y φ
-  | ◇φ => ∃ y, K x y ∧ Forces y φ
-infix:60 " ⊩ " => Forces
+namespace Property
 
-variable {K : KripkeModel} {x : K} {φ ψ χ : Formula α}
+abbrev neg (Φ : Property α K) : Property α K := λ a => ∼(Φ a)
+prefix:80 "∼ₚ" => neg
 
-@[grind =] lemma forces_neg : (x ⊩ ∼φ) ↔ ¬(x ⊩ φ) := by induction φ generalizing x <;> grind;
-@[grind =] lemma forces_imp : (x ⊩ φ ➝ ψ) ↔ (x ⊩ φ → x ⊩ ψ) := by grind;
-@[grind =] lemma forces_iff : (x ⊩ φ ⭤ ψ) ↔ ((x ⊩ φ) ↔ (x ⊩ ψ)) := by grind;
+end Property
 
-@[grind]
-def Valid (K : KripkeModel) (φ : Formula α) := ∀ x : K, x ⊩ φ
-infixl:60 " ⊨ " => Valid
+/-- Statement about `Property` -/
+abbrev Metaproperty (α) (K : Model) := (Property α K) → Formula K
 
-variable {Φ : Predicate α}
 
-@[grind .] lemma valid_axiomK : K ⊨ □(φ ➝ ψ) ➝ (□φ ➝ □ψ) := by intro x; grind;
+section
 
-lemma valid_mDuality : K ⊨ ◇φ ⭤ ∼□(∼φ) := by intro x; grind;
+variable {K : Model} {φ ψ : Formula K}
 
-lemma valid_qDuality : K ⊨ (∃' Φ) ⭤ ∼(∀' ∼Φ) := by intro x; grind;
+@[grind .]
+lemma valid_axiomT [K.IsReflexive] : ⊧ □φ ➝ φ := by
+  intro x hx;
+  apply hx;
+  exact K.refl;
 
-@[grind .] lemma valid_LEM : K ⊨ φ ⋎ ∼φ := by grind;
+@[grind .]
+lemma valid_axiomTDual [K.IsReflexive] : ⊧ φ ➝ ◇φ := by
+  intro x hx;
+  use x;
+  constructor;
+  . exact K.refl;
+  . exact hx;
 
-@[grind =>] lemma valid_mdp (h₁ : K ⊨ φ ➝ ψ) (h₂ : K ⊨ φ) : K ⊨ ψ := by
-  intro x;
-  apply forces_imp.mp $ h₁ x;
-  apply h₂;
+@[grind .]
+lemma valid_axiomFour [K.IsTransitive] : ⊧ □φ ➝ □□φ := by
+  intro x hx y Rxy z Ryz;
+  apply hx;
+  exact K.trans Rxy Ryz;
 
-@[grind =>]
-lemma valid_impTrans (h₁ : K ⊨ φ ➝ ψ) (h₂ : K ⊨ ψ ➝ χ) : K ⊨ φ ➝ χ := by
-  intro x;
-  apply forces_imp.mpr;
-  intro h₃;
-  apply forces_imp.mp $ h₂ x;
-  apply forces_imp.mp $ h₁ x;
-  assumption;
+@[grind .]
+lemma valid_axiomB [K.IsSymmetric] : ⊧ ◇□φ ➝ φ := by
+  intro x hx;
+  obtain ⟨y, Rxy, hy⟩ : (◇□φ) x := hx;
+  apply hy;
+  exact K.symm Rxy;
 
-@[grind <=] lemma valid_necessitation (h : K ⊨ φ) : K ⊨ □φ := by grind;
+@[grind .]
+lemma valid_axiomFive [K.IsEuclidean] : ⊧ ◇□φ ➝ □φ := by
+  intro x hx y Rxy;
+  obtain ⟨z, Rxz, hz⟩ : (◇□φ) x := hx;
+  apply hz;
+  exact K.eucl Rxz Rxy;
 
-@[grind .] lemma valid_axiomT [K.IsReflexive] : K ⊨ □φ ➝ φ := by grind;
+example [K.IsKB4] : ⊧ ◇□φ ➝ □φ := valid_axiomFive
+example [K.IsS5] : ⊧ ◇□φ ➝ □φ := valid_axiomFive
 
-@[grind .] lemma valid_axiomTDual [K.IsReflexive] : K ⊨ φ ➝ ◇φ := by grind;
+@[grind →]
+lemma valid_mdp (h₁ : ⊧ φ ➝ ψ) (h₂ : ⊧ φ) : ⊧ ψ := fun x => h₁ x $ h₂ x
 
-@[grind .] lemma valid_axiomFour [K.IsTransitive] : K ⊨ □φ ➝ □□φ := by grind;
-
-@[grind .] lemma valid_axiomB [K.IsSymmetric] : K ⊨ ◇□φ ➝ φ := by grind;
-
+end
 
 
 section Argument
 
 variable
-  {K : KripkeModel} [K.IsS5]
-  {IsPositive : Predicate α → Formula α}
-  {Ax1a : K ⊨ ∀' P, IsPositive Φ ➝ ∼IsPositive (∼Φ)}
-  {Ax1a : ∀ Φ : Predicate α, K ⊨ IsPositive Φ ➝ ∼IsPositive (∼Φ)}
-  {Ax1b : ∀ Φ : Predicate α, K ⊨ ∼IsPositive Φ ➝ IsPositive (∼Φ)}
-  {Ax2 : ∀ Φ Ψ : Predicate α, K ⊨ IsPositive Φ ⋏ □(∀' (λ x => Φ x → Ψ x)) ➝ IsPositive Ψ}
+  {α} {K : Model}
+  {Φ Ψ : Property α K}
 
-def Godlike : Predicate α := λ x => ∀' (λ P => IsPositive P ➝ P x)
+variable {Positive : Metaproperty α K}
 
-variable
-  {Ax3 : K ⊨ IsPositive Godlike}
+/-- Emptyness is non-positive. -/
+lemma emptyness_negative
+  (Ax1 : ∀ Φ : Property α K, ⊧ Positive (λ a => ∼(Φ a)) ⭤ ∼Positive Φ)
+  (Ax2 : ∀ Φ Ψ : Property α K, ⊧ Positive Φ ➝ □(∀' (λ a => Φ a ➝ Ψ a)) ➝ Positive Ψ)
+  : ⊧ ∼Positive (λ _ => ⊥ₘ) := by
+  intro x;
 
-variable
-  {Ax4 : ∀ φ : Predicate α, K ⊨ IsPositive φ ➝ □(IsPositive φ)}
+  by_contra! hC;
+  replace hC : x ∈ Positive (λ _ => ⊥ₘ) := not_not.mp $ Set.mem_compl_iff _ _ |>.not.mp hC;
 
-abbrev NecessaryExistence : Predicate α := by sorry;
+  have : x ∈ ∼Positive (λ _ => ⊤ₘ) := Ax1 _ x |>.mp (by simpa);
+  have : x ∈ Positive (λ _ => ⊤ₘ) := Ax2 (λ _ => ⊥ₘ) _ _ ?_ ?_;
+  contradiction;
+  . assumption;
+  . grind;
 
-variable
-  {Ax5 : K ⊨ IsPositive NecessaryExistence}
+/-- Positive properties are possibly exemplified. -/
+lemma theorem1
+  (Ax1 : ∀ Φ : Property α K, ⊧ Positive (λ a => ∼(Φ a)) ⭤ ∼Positive Φ)
+  (Ax2 : ∀ Φ Ψ : Property α K, ⊧ Positive Φ ➝ □(∀' (λ a => Φ a ➝ Ψ a)) ➝ Positive Ψ)
+  : ⊧ Positive Φ ➝ ◇∃' (λ a => Φ a) := by
+  intro x hx;
+  have : (∼□∀'(λ a => Φ a ➝ ⊥ₘ)) x := (not_imp_not.mpr $ Ax2 Φ (λ _ => ⊥ₘ) x hx) $ by apply emptyness_negative Ax1 Ax2;
+  grind;
+
+def Godlike (Positive : Metaproperty α K) : Property α K := λ a => ∀' (λ Φ => Positive Φ ➝ Φ a)
+
+/-- Possibly, God exists. -/
+lemma possibly_exists_godlike
+  (Ax1 : ∀ Φ : Property α K, ⊧ Positive (λ a => ∼(Φ a)) ⭤ ∼Positive Φ)
+  (Ax2 : ∀ Φ Ψ : Property α K, ⊧ Positive Φ ➝ □(∀' (λ a => Φ a ➝ Ψ a)) ➝ Positive Ψ)
+  (Ax3 : ⊧ Positive (Godlike (Positive)))
+  : ⊧ ◇(∃' (λ (a : α) => Godlike (Positive := Positive) a)) := by
+  intro x;
+  apply theorem1 Ax1 Ax2;
+  apply Ax3;
+
+def Essence (Φ : Property α K) : Property α K := λ a => Φ a ⋏ ∀' (λ Ψ : Property α K => Ψ a ➝ □(∀' (λ b => Φ b ➝ Ψ b)))
+
+/-- Being Godlike is an essence of any Godlike being -/
+lemma godlike_ess
+  (Ax1 : ∀ Φ : Property α K, ⊧ Positive (λ a => ∼(Φ a)) ⭤ ∼Positive Φ)
+  (Ax4 : ∀ Φ : Property α K, ⊧ Positive Φ ➝ □Positive Φ)
+  : ⊧ ∀' (λ a => Godlike (Positive := Positive) a ➝ Essence (Godlike (Positive := Positive)) a) := by
+  intro x a ha;
+  constructor;
+  . assumption;
+  . intro Ψ hΨ y Rxy b b_godlike_y;
+    have Ψ_positive_x : x ∈ Positive Ψ := by
+      by_contra! hC;
+      have : Positive (∼Ψ ·) x := Ax1 Ψ x |>.mpr hC;
+      have : (∼Ψ a) x := ha _ this;
+      contradiction;
+    apply b_godlike_y;
+    apply Ax4 _ x Ψ_positive_x y Rxy;
+
+def NecessaryExistence : Property α K := λ a => ∀' (λ Φ : Property α K => Essence Φ a ➝ □∃' (λ b => Φ b))
+
+theorem necessary_exists_godlike
+  [K.IsS5]
+  (Ax1 : ∀ Φ : Property α K, ⊧ Positive (λ a => ∼(Φ a)) ⭤ ∼Positive Φ)
+  (Ax2 : ∀ Φ Ψ : Property α K, ⊧ Positive Φ ➝ □(∀' (λ a => Φ a ➝ Ψ a)) ➝ Positive Ψ)
+  (Ax3 : ⊧ Positive (Godlike (Positive := Positive)))
+  (Ax4 : ∀ Φ : Property α K, ⊧ Positive Φ ➝ □Positive Φ)
+  (Ax5 : ⊧ Positive NecessaryExistence)
+  : ⊧ □∃' (λ a => Godlike (α := α) (K := K) (Positive := Positive) a) := by
+  suffices ⊧ ◇□∃' (λ a => Godlike (Positive := Positive) a) by apply valid_mdp valid_axiomFive this;
+  intro x;
+  obtain ⟨y, Rxy, ⟨a, a_godlike_y⟩⟩ : (◇∃'(λ a => Godlike _ a)) x := possibly_exists_godlike Ax1 Ax2 Ax3 x;
+  use y;
+  constructor;
+  . assumption;
+  . have H₁ := a_godlike_y NecessaryExistence;
+    have : NecessaryExistence a y := H₁ $ Ax5 y;
+    apply this;
+    apply godlike_ess Ax1 Ax4 y a a_godlike_y;
 
 end Argument
-
 
 
 end GodExistence
